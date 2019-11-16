ba28114b6de3e58e4611a1de759f33c0018ce06b
Chia-Hung Lin <clin4j@googlemail.com>
2019-11-16 16:04:43 +0100

    * Add zio 1.0.0-RC16 * Add InternalQueue that holds unbounded zio.Queue * Add Supervisor test case

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.12.10"
 libraryDependencies ++= Seq(
   "com.google.guava" % "guava" % "28.1-jre",
   "com.typesafe" % "config" % "1.4.0",
+  "dev.zio" %% "zio" % "1.0.0-RC16",
   "org.slf4j" % "slf4j-api" % "1.7.29",
   "org.slf4j" % "slf4j-simple" % "1.7.29",
   "org.scalatest" %% "scalatest" % "3.0.8"
--- src/main/scala/supervision/Supervisor.scala
+++ src/main/scala/supervision/Supervisor.scala
@@ -1,13 +1,16 @@
 package supervision
 
-import supervision._
-import scala.collection.immutable.Queue
+import zio.Queue
+import zio.UIO
+import scala.util.Random
 
 trait Supervisor[S] {
 
   def id(): Identifier
 
   def name(): String
+
+  def state(): Supervisor.State
 
   def withService(service: S): Supervisor[S]
 
@@ -24,54 +27,69 @@ object Supervisor {
   protected[supervision] case object Paused extends State
   protected[supervision] case class Terminated(id: Identifier) extends State
 
+  protected[supervision] case class InternalQueue[T](queue: UIO[Queue[T]] = Queue.unbounded[T]) {
+
+    def enqueue(data: T): InternalQueue[T] = InternalQueue(for {
+      q <- queue
+      _ <- q.offer(data)
+    } yield q)
+
+    def dequeue(): UIO[T] = for {
+      q <- queue
+      v <- q.take
+    } yield v
+
+  }
 
   def apply[S <: Service[_]](
-    value: String,
+    supervisorName: String = Random.alphanumeric.take(10).mkString(""),
     spec: Spec = Spec(),
-    state: State = Stopped,
+    supervisorState: State = Stopped,
     services: Map[Identifier, S] = Map.empty[Identifier, S],
     offlineServices: Map[Identifier, S] = Map.empty[Identifier, S],
-    restartQeueu: Queue[Identifier] = Queue.empty[Identifier]
+    restartQueue: InternalQueue[Identifier] = InternalQueue[Identifier]()
   )(
-    implicit s: (String, Spec, State, Map[Identifier, S], Map[Identifier, S], Queue[Identifier]) => Supervisor[S]
-  ): Supervisor[S] = s(value, spec, state, services, offlineServices, restartQeueu)
+    implicit s: (String, Spec, State, Map[Identifier, S], Map[Identifier, S], InternalQueue[Identifier]) => Supervisor[S]
+  ): Supervisor[S] = s(supervisorName, spec, supervisorState, services, offlineServices, restartQueue)
 
   implicit def supervisor[S <: Service[_]]:
-    (String, Spec, State, Map[Identifier, S], Map[Identifier, S], Queue[Identifier] ) => Supervisor[S] =
+    (String, Spec, State, Map[Identifier, S], Map[Identifier, S], InternalQueue[Identifier]) => Supervisor[S] =
   (
-    value: String,
+    supervisorName: String,
     spec: Spec,
-    state: State,
+    supervisorState: State,
     services: Map[Identifier, S],
     offlineServices: Map[Identifier, S],
-    restartQeueu: Queue[Identifier]
+    restartQueue: InternalQueue[Identifier]
   ) => new Supervisor[S] {
 
-    override def id(): Identifier = Identifier.create(value)
+    override def id(): Identifier = Identifier.create(name())
 
-    override def name(): String = "Supervisor"
+    override def name(): String = supervisorName
+
+    override def state(): Supervisor.State = supervisorState
 
     override def withService(service: S): Supervisor[S] =
       supervisor[S](
-        value,
+        supervisorName,
         spec,
-        state,
+        supervisorState,
         services ++ Map(service.id -> service),
         offlineServices,
-        restartQeueu.enqueue(service.id)
+        restartQueue.enqueue(service.id)
       )
 
-    def start(): Either[Throwable, Supervisor[S]] = state match {
+    def start(): Either[Throwable, Supervisor[S]] = supervisorState match {
       case Stopped => Right(supervisor[S](
-        value,
+        supervisorName,
         spec,
-        state,
+        supervisorState,
         services.map { case (id, service) =>
           service.start()
           (id, service)
         },
         offlineServices,
-        restartQeueu
+        restartQueue
       ))
       case Running => Right(this)
       case Terminated(value) => Left(SupervisorTerminated(Terminated(value)))
